e3384a4a2ca534eaf1c4d9ced5efa03bb6dbcf5d
girishjain13 <123942958+girishjain13@users.noreply.github.com>
2023-12-14 11:16:33 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,45 @@
+// Initialize Boxever
+Boxever.init({
+    "client_key": "YOUR_CLIENT_KEY",
+    "target": "api.boxever.com/v1.2",
+    "session": Boxever.generateSessionID(),
+    "pointOfSale": "web",
+    "channel": "WEB"
+});
+
+// Get the form element
+const form = document.getElementById('loginForm');
+
+// Add an event listener for form submission
+form.addEventListener('submit', function(event) {
+    // Prevent the form from being submitted
+    event.preventDefault();
+
+    // Get the input values
+    const fname = document.getElementById('fname').value;
+    const lname = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+
+    // Define the identity event
+    const identityEvent = {
+        browser_id: Boxever.getID(),
+        channel: "WEB",
+        type: "IDENTITY",
+        language: "EN",
+        currency: "EUR",
+        page: "login",
+        pos: "myretailsite/ireland",
+        identifiers: [
+            {
+                id: email,
+                provider: "BXLP"
+            }
+        ],
+        email: email,
+        firstname: fname,
+        lastname: lname
+    };
+
+    // Send the identity event
+    Boxever.eventCreate(identityEvent, function(data){}, 'json');
+});
